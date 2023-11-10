555d0a4a6f024b6cdbb2e842a72977d5c50b971f
Matt Rollender <matt@rollender.com>
2023-11-10 14:56:02 -0500

    overlay -> pkg

--- flake.nix
+++ flake.nix
@@ -7,35 +7,34 @@
   outputs = { self, nixpkgs, rose-pine, ...}:
     let 
       system = "x86_64-linux";
-      vimPluginsOverlay = final: prev: {
-        vimPlugins = prev.vimPlugins.extend (final': prev': {
-          rose-pine = prev.vimUtils.buildVimPlugin {
+      pkgs = nixpkgs.legacyPackages.${system};
+        vimPlugins = pkgs.vimPlugins.extend (final': prev': {
+          rose-pine = pkgs.vimUtils.buildVimPlugin {
             name = "rose-pine";
             src = rose-pine;
           };
           nvim-treesitter = (prev'.nvim-treesitter.withPlugins (plugins: prev.tree-sitter.allGrammars));
-          mrr-config = prev.vimUtils.buildVimPlugin {
+          mrr-config = pkg.vimUtils.buildVimPlugin {
             name = "mrr-config";
             src = ./src;
           };
         });
-        neovim = prev.neovim // {
+        neovim = pkgs.neovim // {
           # TODO: requiring feels unnecessary
           extraLuaConfig = ''
             require("mrr");
           '';
           plugins = [
-            final.vimPlugins.telescope-nvim
-            final.vimPlugins.nvim-lspconfig
-            final.vimPlugins.nvim-cmp
-            final.vimPlugins.rose-pine
-            final.vimPlugins.nvim-treesitter
-            final.vimPlugins.mrr-config
-            final.vimPlugins.vim-tmux-navigator
+            vimPlugins.telescope-nvim
+            vimPlugins.nvim-lspconfig
+            vimPlugins.nvim-cmp
+            vimPlugins.rose-pine
+            vimPlugins.nvim-treesitter
+            vimPlugins.mrr-config
+            vimPlugins.vim-tmux-navigator
           ];
-        };
       };
     in {
-      overlays.${system}.default = vimPluginsOverlay;
+      legacyPackages.${system}.default = vimPluginsOverlay;
     };
 }
