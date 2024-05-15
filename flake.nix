a548d82351f4115d05ef80e8c67a965840442011
Matt Rollender <matt@rollender.com>
2024-05-14 20:44:39 -0400

    update nixfmt

--- flake.nix
+++ flake.nix
@@ -4,21 +4,35 @@
     url = "github:rose-pine/neovim";
     flake = false;
   };
-  inputs.nil = { url = "github:oxalica/nil"; };
+  inputs.nil = {
+    url = "github:oxalica/nil";
+  };
   inputs.conform = {
     url = "github:stevearc/conform.nvim";
     flake = false;
   };
-  outputs = { self, nixpkgs, rose-pine, nil, conform, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rose-pine,
+      nil,
+      conform,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      nixosModules."home-manager" = (import ./modules/home-manager.nix {
-        inherit rose-pine;
-        inherit conform;
-        nil = nil.packages.${system}.default;
-        custom-plugin-src = ./src;
-      });
+    in
+    {
+      nixosModules."home-manager" = (
+        import ./modules/home-manager.nix {
+          inherit rose-pine;
+          inherit conform;
+          nil = nil.packages.${system}.default;
+          custom-plugin-src = ./src;
+        }
+      );
+      formatter = pkgs.nixfmt-rfc-style;
     };
 }
