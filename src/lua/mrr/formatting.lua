95e1ad976be031a0c6c8d2341da472fa04eeffb8
Matt Rollender <matt@rollender.com>
2024-06-13 22:30:38 -0400

    simplfy formatting cfg

--- src/lua/mrr/formatting.lua
+++ src/lua/mrr/formatting.lua
@@ -4,30 +4,19 @@ vim.bo.softtabstop = 0
 vim.bo.tabstop = 8 -- make it obvious if there's any tab funny business
 vim.bo.shiftwidth = 2
 
---[[
-require('formatter').setup {
-	logging = true,
-	log_level = vim.log.levels.INFO,
-	filetype = {
-		nix = { require('formatter.filetypes.nix').nixfmt },
-	}
-}
---]]
 
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    nix = { "nixfmt" }
-  },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  }
+	formatters_by_ft = {
+		lua = { "stylua" },
+		nix = { "nixfmt" }
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	}
 })
 
-vim.api.nvim_clear_autocmds({ group = format_group })
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = format_group,
-  command = ":FormatWrite"
+require("conform").formatters.stylua({
+	prepend_args = { "--indent-type=Spaces" },
 })
