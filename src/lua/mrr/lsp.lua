4a6e2bfe2559813e49e6682da719fea5fdfc5c87
Matt Rollender <matt@rollender.com>
2024-06-15 19:55:47 -0400

    clean comment

--- src/lua/mrr/lsp.lua
+++ src/lua/mrr/lsp.lua
@@ -1,21 +1,6 @@
 local lspconfig = require("lspconfig")
 local format_group = vim.api.nvim_create_augroup("__formatter__", {})
 
--- formatting now in mrr.formatting
---[[
-local on_attach = function(client, bufnr)
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_clear_autocmds({ group = format_group, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = format_group,
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.buf.format()
-			end,
-		})
-	end
-end
---]]
 lspconfig.pyright.setup({
     on_attach = on_attach,
 })
